9477983dca1639655e664562536895073e571434
Anuj-Khadka <anujkhadka008@gmail.com>
2023-02-09 12:18:55 +0545

    add frontedn

---
+++ js/script.js
@@ -0,0 +1,81 @@
+let blurCounter = 1;
+let pasteCounter = 1;
+let blurTime = 0;
+let essaletea = document.getElementById("essayArea");
+let submitEssay = document.getElementById("submitEssay");
+let wordCount = document.getElementById("wordCount");
+
+window.addEventListener("focus", (event) => {
+  console.log("tab is active");
+});
+window.addEventListener("blur", (event) => {
+  console.log("tab is inactive");
+  let date = new Date();
+  console.log(date);
+  let alertMsg = `
+    <div class="alert alert-secondary" role="alert">
+        We tried to switch the tab. Information recorded! ${blurCounter}
+    </div>
+`;
+  blurCounter++;
+  let container = document.getElementById("container");
+  container.insertAdjacentHTML("afterbegin", alertMsg);
+//   setInterval(() => {
+//     blurTime++;
+//     console.log(blurTime);
+//   }, 1000);
+});
+
+// Run myfunc every second
+let myfunc = setInterval(function () {
+  let now = new Date().getTime();
+  let countDownDate = new Date(`Thu Feb 09 2023 00:45:05`).getTime();
+  let timeleft = countDownDate - now;
+
+  // Calculating the minutes and seconds left
+  let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+  if (minutes <= 30) {
+    essayArea.removeAttribute("disabled");
+    submitEssay.removeAttribute("disabled");
+  }
+
+  document.getElementById("timer").innerText = `${minutes} : ${seconds}`;
+
+  // Display the message when countdown is over
+  if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.getElementById("timer").innerHTML = "TIME UP!!";
+  }
+}, 1000);
+
+
+essayArea.addEventListener("paste", () => {
+  let pasteMsg = `
+    <div class="alert alert-secondary" role="alert">
+        We tried to paste something. Information recorded! ${pasteCounter}
+    </div>
+`;
+  pasteCounter++;
+  let container = document.getElementById("container");
+  container.insertAdjacentHTML("afterbegin", pasteMsg);
+});
+
+submitEssay.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let submitMsg = `
+    <div class="alert alert-primary" role="alert">
+        The essay has been received. thanks for you submission.
+    </div>
+`;
+  let container = document.getElementById("container");
+  container.insertAdjacentHTML("afterbegin", submitMsg);
+});
+
+
+// word count
+essayArea.addEventListener("input", function () {
+  var words = essayArea.value.split(/\s+/);
+  wordCount.innerHTML = words.length;
+});
